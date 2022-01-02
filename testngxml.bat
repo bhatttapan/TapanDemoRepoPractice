9b34e2975aedc0a8291cf1ad4488cd649b46b3d9
bhatttapan <bhatt.tapan1@gmail.com>
2022-01-01 19:30:12 -0500

    entire path has been changed

--- testngxml.bat
+++ testngxml.bat
@@ -1,5 +1,5 @@
 set projectLocation=C:\TAPAN\STUDY\QUALITY ASSURANCE NIKUL SIR\Selenium_Workspace\PracticeBasicFramework3
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
+C:\TAPAN\STUDY\QUALITY ASSURANCE NIKUL SIR\Selenium_Workspace\PracticeBasicFramework3\testng.xml
 pause
