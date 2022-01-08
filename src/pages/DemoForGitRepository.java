ae0bf2a2ebde87a72d54fba77841eb984a49d307
bhatttapan <bhatt.tapan1@gmail.com>
2022-01-08 16:38:10 -0500

    hello pr request

--- src/pages/DemoForGitRepository.java
+++ src/pages/DemoForGitRepository.java
@@ -8,7 +8,7 @@ public class DemoForGitRepository {
 		System.out.println("this is example for pull event");
 		System.out.println("Hi i am reinitializing git for the second time");
 		System.out.println("second change for pull command in git");
-        System.out.println("Hi new year","new year gonna be awesome dont worry");
+        System.out.println("Hi new year new year gonna be awesome dont worry");
         System.out.println("going to integrate github in jenkins tool");
         System.out.println("working on git pull request in compny");
 	}
