3538d01974647c43d6b5ae6a5544b136fd37bc12
bhatttapan <93291859+bhatttapan@users.noreply.github.com>
2022-01-08 20:25:42 -0500

    Update DemoForGitRepository.java

--- src/pages/DemoForGitRepository.java
+++ src/pages/DemoForGitRepository.java
@@ -5,7 +5,7 @@ public class DemoForGitRepository {
 	public void demoGit()
 	{
 		System.out.println("This is example page to add git repository");
-		System.out.println("this is example for pull event");
+		
 		System.out.println("Hi i am reinitializing git for the second time");
 		System.out.println("second change for pull command in git");
         System.out.println("New year gonna be awesome dont worry");
